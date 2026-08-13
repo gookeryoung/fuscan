83e0adbea1d9e4e249456b2c6a7051ab9b94c2ea
gookeryoung <gooker_young@qq.com>
2026-08-13 14:30:56 +0800

    chore: 更新版本 0.3.3 → 0.3.4

--- src/fuscan/__init__.py
+++ src/fuscan/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __all__ = ["__author__", "__description__", "__license__", "__version__"]
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "gooker_young"
 __license__ = "MIT"
 __description__ = "极速通用文件扫描器"
